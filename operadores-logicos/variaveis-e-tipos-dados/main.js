35c850f4d8b4d1e7a8efc555d9370e37be6ad09c
Cristian Pietro <crpietro26@gmail.com>
2023-01-20 13:29:12 -0300

    logica de programação em js

--- operadores-logicos/variaveis-e-tipos-dados/main.js
+++ operadores-logicos/variaveis-e-tipos-dados/main.js
@@ -4,3 +4,10 @@ usuario1 = parseFloat(usuario1);
 let usuario2 = prompt('digite um numero');
 usuario2 = parseFloat(usuario2);
 
+let media = (usuario1 + usuario2) /2;
+
+if(media > 5 && usuario1 > 0 && usuario2 > 0){
+    alert('APROVADOS')
+}else{
+    alert('reprovados')
+}
